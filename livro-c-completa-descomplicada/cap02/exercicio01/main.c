0ce482454e46b58b6786accbe46a28d1e61ea29c
João Paulo <jppo@live.com>
2023-07-10 21:20:21 -0300

    Resolução do primeiro exercício

---
+++ livro-c-completa-descomplicada/cap02/exercicio01/main.c
@@ -0,0 +1,10 @@
+// Exercício: Elabore um programa que escreva as as mensagens "Inicio do programa" e "Fim" na tela, uma em cada linha, usando apenas um printf
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){  
+printf("Inicio do programa \nFim \n");
+
+system("pause");
+return 0;
+}
